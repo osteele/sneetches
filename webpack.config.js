b68fc11b51864039b86f4c55ac121dc4aad3638f
Oliver Steele <steele@osteele.com>
2018-08-10 21:20:33 -0400

    Fix webpack eval incompatibility with chrome security policy

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,38 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        options: path.join(__dirname, './src/options.ts'),
-        content: path.join(__dirname, './src/content.ts')
-    },
-    output: {
-        path: path.join(__dirname, './build'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-        new CleanWebpackPlugin('build'),
-        new CopyWebpackPlugin([
-            { from: 'src', ignore: ['*.js', '*.ts', '.DS_Store'] }
-        ])
+  entry: {
+    options: path.join(__dirname, './src/options.ts'),
+    content: path.join(__dirname, './src/content.ts')
+  },
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin('build', { verbose: false }),
+    new CopyWebpackPlugin([
+      { from: 'src', ignore: ['*.js', '*.ts', '.DS_Store'] }
+    ])
+  ]
 };
 
 switch (process.env.NODE_ENV) {
-    case 'development':
-        // module.exports.devtool = 'inline-source-map';
-        break;
-    case 'production':
-        break;
+  case 'development':
+    module.exports.devtool = 'inline-source-map';
+    break;
+  case 'production':
+    break;
 }
